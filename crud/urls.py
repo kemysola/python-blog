98aae60e81d1e72d34c705271b6faec5232b667f
Adekemi Awomolo <dakemilola@gmail.com>
2021-04-26 04:34:01 +0100

    removedspace

--- crud/urls.py
+++ crud/urls.py
@@ -21,8 +21,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myblogs.urls')),
-    path('articles/',include('articles.urls')),
-    path('accounts/',include('accounts.urls, namespace='accounts')) 
+    path('articles/',include('articles.urls)),
+    path('accounts/',include('accounts.urls')),
     path('users/', include('users.urls', namespace='users')),
     #path('', include('articles.urls'))
 ]
