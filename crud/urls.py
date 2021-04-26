ac0d6e746077f2c4fd4942b6f7360db7c8c94950
Adekemi Awomolo <dakemilola@gmail.com>
2021-04-26 04:30:54 +0100

    asd

--- crud/urls.py
+++ crud/urls.py
@@ -21,7 +21,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myblogs.urls')),
-    path('articles/',include('articles.urls, namespace='articles')),
+    path('articles/',include('articles.urls')),
     path('accounts/',include('accounts.urls, namespace='accounts')) 
     path('users/', include('users.urls', namespace='users')),
     #path('', include('articles.urls'))
